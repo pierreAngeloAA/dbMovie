8e5e5fb77e4a6138817ddb38ad89b3e2a97423b3
Pierre Algarin <pierrealgarin@gmail.com>
2025-06-18 07:26:03 -0500

    Implementar favoritos con localStorage y actualizar estilos del frontend

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,68 @@
 const API_KEY = 'a193a1e35492fef05a07b98c1844f80c';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-// se apunta hacia los div donde se intrudira la informacion.
-const favorite = document.getElementById('favorites-movies')
+// Referencias a los contenedores en el HTML
+const favorite = document.getElementById('favorites-movies');
 const popularContainer = document.getElementById('popular-movies');
 const topContainer = document.getElementById('top-movies');
 
-//funcion que se reutilizara con los tres tipos de contenedores.
+// Objeto localStorage
+var movies_fav = JSON.parse(localStorage.getItem("favorites")) || [];
+
+// Obtener películas desde la API
 function fetchMovies(endpoint, type_container) {
-  //`https://api.themoviedb.org/3/movie/popular?api_key=a193a1e35492fef05a07b98c1844f80c&language=en-ES`
   fetch(`${BASE_URL}/movie/${endpoint}?api_key=${API_KEY}&language=es-ES`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       data.results.forEach(movie => {
-        const card = document.createElement('div'); // se crea un div por cada elemento del resultado de la consulta a la Api.
-        card.classList.add('movie-card'); //para agregar un clase a los div creados anteriormente.
+        const card = document.createElement('div');
+        card.classList.add('movie-card');
         card.innerHTML = `
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
-          `; //se le agregala imagen de cada elemento, concatenando el path de cada imagen a la base_url de las imagenes.
-              // se tiene en cuenta el w500 que en este caso es el tamaño de la imagen en un rango de 200-500 px.
+          <button class="fav-btn">❤️</button>
+          `;
+          card.querySelector('.fav-btn').addEventListener('click', () => addToFavorites(movie));
         type_container.appendChild(card);
       });
     });
 }
 
+// Agregar película a favoritos
+function addToFavorites(movie){
+  if (!movies_fav.some(item => item.id === movie.id)){
+    movies_fav.push(movie);
+    localStorage.setItem("favorites", JSON.stringify(movies_fav));
+    showfavorites();
+  }else{
+    alert("La pelicula ya existe en tus favoritos");
+  }
+}
+
+//mostrar peliculas favoritas
+function showfavorites(){
+  favorite.innerHTML = '';
+  movies_fav.forEach(movie =>{
+      const card = document.createElement('div');
+      card.classList.add('movie-card');
+      card.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
+        <button class="delete-btn">🤍</button>
+        `;
+        card.querySelector('.delete-btn').addEventListener('click', () => deleteFavorite(movie.id));
+      favorite.appendChild(card);
+  })
+}
+
+//eliminar peliculas favoritas
+function deleteFavorite(id){
+  const index = movies_fav.findIndex(movie => movie.id === id);
+  if (index !== -1) {
+    movies_fav.splice(index, 1);
+    localStorage.setItem("favorites", JSON.stringify(movies_fav));
+    showfavorites();
+  }
+}
+
+showfavorites()
 fetchMovies('popular', popularContainer);
 fetchMovies('top_rated', topContainer);
