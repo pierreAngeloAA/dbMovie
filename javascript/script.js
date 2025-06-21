aba45a6465575d6aea810aa75ffaab6d923c8386
Pierre Algarin <pierrealgarin@gmail.com>
2025-06-20 19:27:22 -0500

    feat: add movie detail modal with styling
    - Implemented modal to display detailed movie information
    - Styled modal content, including title, image, overview, and genre badges
    - Added logic to open modal on image click and close on background click
    - Ensured responsive and centered layout using flexbox

--- javascript/script.js
+++ javascript/script.js
@@ -5,25 +5,42 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const favorite = document.getElementById('favorites-movies');
 const popularContainer = document.getElementById('popular-movies');
 const topContainer = document.getElementById('top-movies');
+const modal = document.getElementById('movie-modal');
+const modalContent = document.getElementById('modal-details');
 
 // Objeto localStorage
 var movies_fav = JSON.parse(localStorage.getItem("favorites")) || [];
+
+// funcion para crear cartas
+function createCard(movies, type_container){
+  const isFavorite = type_container === favorite;
+  const btnClass = isFavorite ? 'delete-btn' : 'fav-btn';
+  const btnIcon = isFavorite ? '🤍' : '❤️';
+    movies.forEach(movie => {
+      const card = document.createElement('div');
+      card.classList.add('movie-card');
+      card.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
+        <button class="${btnClass}">${btnIcon}</button>
+        `;
+        card.querySelector(`.${btnClass}`).addEventListener('click', () => {
+          if (isFavorite) {
+            deleteFavorite(movie.id);
+          } else {
+            addToFavorites(movie);
+          }
+        });
+        card.querySelector('img').addEventListener('click', () => showDetails(movie.id));
+      type_container.appendChild(card);
+    });
+}
 
 // Obtener películas desde la API
 function fetchMovies(endpoint, type_container) {
   fetch(`${BASE_URL}/movie/${endpoint}?api_key=${API_KEY}&language=es-ES`)
     .then(res => res.json())
     .then(data => {
-      data.results.forEach(movie => {
-        const card = document.createElement('div');
-        card.classList.add('movie-card');
-        card.innerHTML = `
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
-          <button class="fav-btn">❤️</button>
-          `;
-          card.querySelector('.fav-btn').addEventListener('click', () => addToFavorites(movie));
-        type_container.appendChild(card);
-      });
+      createCard(data.results, type_container);
     });
 }
 
@@ -39,18 +56,9 @@ function addToFavorites(movie){
 }
 
 //mostrar peliculas favoritas
-function showfavorites(){
+function showfavorites(movies,type_container){
   favorite.innerHTML = '';
-  movies_fav.forEach(movie =>{
-      const card = document.createElement('div');
-      card.classList.add('movie-card');
-      card.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
-        <button class="delete-btn">🤍</button>
-        `;
-        card.querySelector('.delete-btn').addEventListener('click', () => deleteFavorite(movie.id));
-      favorite.appendChild(card);
-  })
+  createCard(movies_fav, favorite);
 }
 
 //eliminar peliculas favoritas
@@ -59,10 +67,31 @@ function deleteFavorite(id){
   if (index !== -1) {
     movies_fav.splice(index, 1);
     localStorage.setItem("favorites", JSON.stringify(movies_fav));
-    showfavorites();
+    showfavorites(movies_fav, favorite);
   }
 }
 
-showfavorites()
+// mostrar detalles de peliculas
+function showDetails(idMovie) {
+   fetch(`${BASE_URL}/movie/${idMovie}?api_key=${API_KEY}&language=es-ES`)
+    .then(res => res.json())
+    .then(movie => {
+      modalContent.innerHTML = `
+          <h2 class="modal-title">${movie.title}</h2>
+          <img class="modal-image" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+          <p class="movie-overview">${movie.overview}</p>
+          <div class="movie-genres">
+            ${movie.genres.map(genre => `<span class="genre">${genre.name}</span>`).join('')}
+          </div>
+      `;
+      modal.classList.remove('hidden');
+    });
+}
+
+modal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
+
+showfavorites(movies_fav, favorite);
 fetchMovies('popular', popularContainer);
 fetchMovies('top_rated', topContainer);
